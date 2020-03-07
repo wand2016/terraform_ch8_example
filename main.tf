3cdb6c7c127550c301c259774cdd19478f1dddac
Daiki Horiyama <d19921207@gmail.com>
2020-03-08 08:21:10 +0900

    LB構築まで

---
+++ main.tf
@@ -0,0 +1,328 @@
+# ----------------------------------------
+# S3
+# ----------------------------------------
+
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket" "private" {
+  bucket = "terraform-practice-wand-20200308"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket = aws_s3_bucket.private.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "private" {
+  bucket = aws_s3_bucket.private.id
+  policy = data.aws_iam_policy_document.private.json
+}
+
+data "aws_iam_policy_document" "private" {
+  # SSE-KMSでない暗号化を禁止
+  statement {
+    sid = "DenyIncorrectEncryptionHeader"
+    effect = "Deny"
+    principals {
+      type = "*"
+      identifiers = ["*"]
+    }
+    actions = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.private.id}/*"]
+    condition {
+      test = "StringNotEquals"
+      variable = "s3:x-amz-server-side-encryption"
+      values = [
+        "aws:kms"
+      ]
+    }
+  }
+  # 暗号化されていないアップロードを禁止
+  statement {
+    sid = "DenyUnEncryptedObjectUploads"
+    effect = "Deny"
+    principals {
+      type = "*"
+      identifiers = ["*"]
+    }
+    actions = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.private.id}/*"]
+    condition {
+      test = "Null"
+      variable = "s3:x-amz-server-side-encryption"
+      values = [
+        "true"
+      ]
+    }
+  }
+}
+
+
+resource "aws_s3_bucket" "public" {
+  bucket = "terraform-practice-wand-20200308-public"
+  acl = "public-read"
+
+  cors_rule {
+    allowed_origins = ["https://example.com"]
+    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "terraform-practice-wand-20200308-log"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect = "Allow"
+    actions = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+    principals {
+      type = "AWS"
+      identifiers = ["582318560864"]
+    }
+  }
+}
+
+output "mykey_arn" {
+  value = aws_kms_key.mykey.arn
+}
+
+
+# ----------------------------------------
+# N/W
+# ----------------------------------------
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_subnet" "public_0" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "example-1a"
+  }
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    Name = "example-1c"
+  }
+}
+
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  gateway_id = aws_internet_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public_0" {
+  subnet_id = aws_subnet.public_0.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_subnet" "private_0" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.65.0/24"
+  availability_zone = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "example_private_1a"
+  }
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.66.0/24"
+  availability_zone = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "example_private_1c"
+  }
+}
+
+resource "aws_route_table" "private_0" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example_private_0"
+  }
+}
+
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example_private_1"
+  }
+}
+
+resource "aws_route_table_association" "private_0" {
+  subnet_id = aws_subnet.private_0.id
+  route_table_id = aws_route_table.private_0.id
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1.id
+}
+
+resource "aws_eip" "nat_gateway_0" {
+  vpc = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_eip" "nat_gateway_1" {
+  vpc = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_nat_gateway" "nat_gateway_0" {
+  allocation_id = aws_eip.nat_gateway_0.id
+  subnet_id = aws_subnet.public_0.id
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_nat_gateway" "nat_gateway_1" {
+  allocation_id = aws_eip.nat_gateway_1.id
+  subnet_id = aws_subnet.public_1.id
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_route" "private_0" {
+  route_table_id = aws_route_table.private_0.id
+  nat_gateway_id = aws_nat_gateway.nat_gateway_0.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "private_1" {
+  route_table_id = aws_route_table.private_1.id
+  nat_gateway_id = aws_nat_gateway.nat_gateway_1.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+# ----------------------------------------
+# LB
+# ----------------------------------------
+
+module "http_sg" {
+  source = "./security_group"
+  name = "http-sg"
+  vpc_id = aws_vpc.example.id
+  port = 80
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "https_sg" {
+  source = "./security_group"
+  name = "https-sg"
+  vpc_id = aws_vpc.example.id
+  port = 443
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "https_redirect_sg" {
+  source = "./security_group"
+  name = "http-redirect-sg"
+  vpc_id = aws_vpc.example.id
+  port = 8080
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+
+resource "aws_lb" "example" {
+  name = "example"
+  load_balancer_type = "application"
+  internal = false
+  idle_timeout = 60
+  enable_deletion_protection = true
+
+  subnets = [
+    aws_subnet.public_0.id,
+    aws_subnet.public_1.id
+  ]
+
+  access_logs {
+    bucket = aws_s3_bucket.alb_log.id
+    enabled = true
+  }
+
+  security_groups = [
+    module.http_sg.security_group_id,
+    module.https_sg.security_group_id,
+    module.https_redirect_sg.security_group_id
+  ]
+}
