8960f67d07bd338033d31f50cc9a287576afb577
Daiki Horiyama <d19921207@gmail.com>
2020-03-09 02:10:18 +0900

    ドメイン名取得、ALIASレコード設定まで

--- main.tf
+++ main.tf
@@ -343,6 +343,22 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-output "public_dns" {
-  value = aws_lb.example.dns_name
+data "aws_route53_zone" "example" {
+  name = "d-horiyama.com"
+}
+
+resource "aws_route53_record" "example" {
+  zone_id = data.aws_route53_zone.example.zone_id
+  name = data.aws_route53_zone.example.name
+  type = "A"
+
+  alias {
+    name = aws_lb.example.dns_name
+    zone_id = aws_lb.example.zone_id
+    evaluate_target_health = true
+  }
+}
+
+output "domain_name" {
+  value = aws_route53_record.example.name
 }
