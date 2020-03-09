f8e5ae3afb2e03429f1db1934164a634f6d1da4e
Daiki Horiyama <d19921207@gmail.com>
2020-03-09 21:54:07 +0900

    リスナールール追加

--- main.tf
+++ main.tf
@@ -438,3 +438,20 @@ resource "aws_lb_target_group" "example" {
 
   depends_on = [aws_lb.example]
 }
+
+
+resource "aws_lb_listener_rule" "example" {
+  listener_arn = aws_lb_listener.https.arn
+  priority = 100
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
+}
